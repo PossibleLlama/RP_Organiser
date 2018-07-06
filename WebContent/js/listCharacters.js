c5e8588eb46b442092762b000a903b90d037e391
Llama <possiblellama@gmail.com>
2018-07-06 21:13:27 +0100

    Changed the formatting on the array. Adding in 2 additional fields as well.

--- WebContent/js/listCharacters.js
+++ WebContent/js/listCharacters.js
@@ -1,14 +1,14 @@
 // Add get requests based on search criteria.
 module.exports = function(app) {
-    listFromMongo = [
-        {
-            characterName : 'Torq',
-            playerName : 'Matthew',
-            characterClass : [{fighter : 1}],
-            characterRace : 'Half Orc',
-            description : 'I like funnel cakes. Hit \'em with the pointy end.'
-        }
-    ];
+    listFromMongo = [{
+        characterName : 'Torq',
+        playerName : 'Matthew',
+        group: 'Critical Hit',
+        campaign: ['Four Against the Void', 'Celestial Crusade', 'Lords of the Feywild'],
+        characterClass : [{fighter : 1}],
+        characterRace : 'Half Orc',
+        description : 'I like funnel cakes. Hit \'em with the pointy end.'
+    }];
 
     app.get('/api/v1/characters', (req, res) => {
         res.send(listFromMongo);
