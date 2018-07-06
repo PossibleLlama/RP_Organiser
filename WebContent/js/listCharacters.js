d19ae09576b90b87a08ed4e948224d33bf7b773c
Llama <possiblellama@gmail.com>
2018-07-06 10:56:10 +0100

    Adding in a rest endpoint.

--- WebContent/js/listCharacters.js
+++ WebContent/js/listCharacters.js
@@ -1,1 +1,16 @@
 // Add get requests based on search criteria.
+module.exports = function(app) {
+    listFromMongo = [
+        {
+            characterName : 'Torq',
+            playerName : 'Matthew',
+            characterClass : [{fighter : 1}],
+            characterRace : 'Half Orc',
+            description : 'I like funnel cakes. Hit \'em with the pointy end.'
+        }
+    ];
+
+    app.get('/api/v1/characters', (req, res) => {
+        res.send(listFromMongo);
+    })
+}
--- WebContent/server.js
+++ WebContent/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
+var listCharacter = require('./js/listCharacters')(app);
+
 app.use(express.static('html'));
 var server = app.listen(3000, () => {
-    console.log('Server is on port', server.address().port)    
+    console.log('Server is on port', server.address().port)
 });
