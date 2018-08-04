139e3d3a7062e58435c0bfe8c2b70b38ab1c8c69
Llama <possiblellama@gmail.com>
2018-08-04 12:40:44 +0100

    Renamed.

--- WebContent/server.js
+++ WebContent/server.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
 
-var listCharacter = require('./js/listCharacters')(app, mongoose);
+var listCharacter = require('./js/characters')(app, mongoose);
 
 app.use(express.static('html'));
 var server = app.listen(3000, () => {
