85de0e97be25396f77a3adcfc73ae5b0a3dbf0dc
Llama <possiblellama@gmail.com>
2019-01-29 21:52:42 +0000

    Basic create Character test. Deleted application start test.

---
+++ api/rpls.characters/src/test/java/org/kaccag/rpls/characters/character/CharacterTest.java
@@ -0,0 +1,11 @@
+package org.kaccag.rpls.characters.character;
+
+import org.junit.Test;
+
+public class CharacterTest {
+
+    @Test
+    public void createCharacter() {
+        Character initial = new Character("player", "character");
+    }
+}
